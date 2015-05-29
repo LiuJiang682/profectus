d671e04fda7b101a24b24d7838c7ed10d1507aad
Jiang Liu <liujiang682@gmail.com>
2015-05-29 22:49:43 +1000

    Second check for controller

--- invoices/src/main/java/com/profectus/invoices/controller/InvoicesController.java
+++ invoices/src/main/java/com/profectus/invoices/controller/InvoicesController.java
@@ -1,5 +1,9 @@
 package com.profectus.invoices.controller;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,6 +11,8 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.profectus.invoices.model.InvoiceModel;
 
 @Controller
 public class InvoicesController {
@@ -25,10 +31,23 @@ public class InvoicesController {
 	public ModelAndView searchByInvoiceNumber(HttpServletRequest request, HttpServletResponse response) {
 		String invoiceNumber = request.getParameter("invoiceNumber");
 		LOGGER.debug("invoiceNumber: " + invoiceNumber);
-		String message = "SearchByInvoiceNumber ";
-		return new ModelAndView("welcome", "message", message + invoiceNumber);
+		List<InvoiceModel> invoices = getDummies();
+		return new ModelAndView("result", "invoices", invoices);
 	}
 	
+	private List<InvoiceModel> getDummies() {
+		List<InvoiceModel> invoices = new ArrayList<>();
+		InvoiceModel invoice = new InvoiceModel();
+		invoice.setInvoiceNumber(1);
+		invoice.setInvoiceType("Cash");
+		invoice.setInvoiceDate("27/05/2015");
+		invoice.setTotalAmount(new BigDecimal(330));
+		invoice.setNetAmount(new BigDecimal(300));
+		invoice.setCashMethod("Supplied");
+		invoices.add(invoice);
+		return invoices;
+	}
+
 	@RequestMapping("/searchInvoiceType")
 	public ModelAndView searchByInvoiceType() {
 		String message = "SearchByInvoiceType";
