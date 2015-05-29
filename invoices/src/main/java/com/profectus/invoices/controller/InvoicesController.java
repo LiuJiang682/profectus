ff035fef591df9f0166acad794e2ef74d9a8cb52
Jiang Liu <liujiang682@gmail.com>
2015-05-30 02:06:30 +1000

    Four check for controller

--- invoices/src/main/java/com/profectus/invoices/controller/InvoicesController.java
+++ invoices/src/main/java/com/profectus/invoices/controller/InvoicesController.java
@@ -1,6 +1,7 @@
 package com.profectus.invoices.controller;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,25 +9,67 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.profectus.invoices.entity.Invoice;
 import com.profectus.invoices.model.InvoiceModel;
+import com.profectus.invoices.services.InvoicesServices;
 
 @Controller
 public class InvoicesController {
 
-	private static final Logger LOGGER = Logger.getLogger(InvoicesController.class);
-	
+	private static final Logger LOGGER = Logger
+			.getLogger(InvoicesController.class);
+
+	@Autowired
+	private InvoicesServices invoicesServices;
+
 	@RequestMapping("/searchInvoiceNumber")
-	public ModelAndView searchByInvoiceNumber(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView searchByInvoiceNumber(HttpServletRequest request,
+			HttpServletResponse response) {
 		String invoiceNumber = request.getParameter("invoiceNumber");
 		LOGGER.debug("invoiceNumber: " + invoiceNumber);
-		List<InvoiceModel> invoices = getDummies();
+
+		List<InvoiceModel> invoices = new ArrayList<>();
+		try {
+			Invoice invoiceEntity = invoicesServices.find(Long
+					.parseLong(invoiceNumber));
+			InvoiceModel invoice = doEntityToModelConvertion(invoiceEntity);
+			invoices.add(invoice);
+		} catch (Exception e) {
+			LOGGER.error("Query failed.", e);
+		}
 		return new ModelAndView("result", "invoices", invoices);
 	}
-	
+
+	private InvoiceModel doEntityToModelConvertion(Invoice invoiceEntity) {
+		InvoiceModel invoice = new InvoiceModel();
+		invoice.setInvoiceNumber(invoiceEntity.getInvoiceNumber());
+		invoice.setInvoiceType("c".equals(invoiceEntity.getInvoiceType()) ? "Cash"
+				: "Security");
+		// invoice.setInvoiceDate(doDateToStringConvertion(invoiceEntity.getInvoiceDate()));
+		invoice.setInvoiceDate(invoiceEntity.getInvoiceDate());
+		invoice.setTotalAmount(invoiceEntity.getTotalAmount());
+		invoice.setNetAmount(invoiceEntity.getNetAmount());
+		String cashMethod = invoiceEntity.getCashMethod();
+		if (!StringUtils.isEmpty(cashMethod)) {
+			invoice.setCashMethod("c".equals(cashMethod) ? "Collected"
+					: "Supplied");
+		}
+		invoice.setSecurityFee(invoiceEntity.getSecurityFee());
+		return invoice;
+	}
+
+	// private String doDateToStringConvertion(String invoiceDate) {
+	// SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	// return sdf.format(invoiceDate);
+	// }
+
 	private List<InvoiceModel> getDummies() {
 		List<InvoiceModel> invoices = new ArrayList<>();
 		InvoiceModel invoice = new InvoiceModel();
@@ -41,11 +84,20 @@ public class InvoicesController {
 	}
 
 	@RequestMapping("/searchInvoiceType")
-	public ModelAndView searchByInvoiceType(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView searchByInvoiceType(HttpServletRequest request,
+			HttpServletResponse response) {
 		String invoiceType = request.getParameter("invoiceType");
 		String pageSize = request.getParameter("pageSize");
 		LOGGER.debug("invoiceType : " + invoiceType + ", pageSize: " + pageSize);
-		List<InvoiceModel> invoices = getDummies();
+
+		List<InvoiceModel> invoices = new ArrayList<>();
+		List<Invoice> entities = invoicesServices.findByInvoiceType(
+				invoiceType, Integer.parseInt(pageSize));
+		for (Invoice entity : entities) {
+			InvoiceModel invoice = doEntityToModelConvertion(entity);
+			invoices.add(invoice);
+		}
+
 		return new ModelAndView("result", "invoices", invoices);
 	}
 }
