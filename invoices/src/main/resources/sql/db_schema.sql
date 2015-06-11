1bc8d89d2e144a6ceba9884d9415af8a1774b35a
Jiang Liu <liujiang682@gmail.com>
2015-06-11 16:49:59 +1000

    Refactor the code.

--- invoices/src/main/resources/sql/db_schema.sql
+++ invoices/src/main/resources/sql/db_schema.sql
@@ -1,7 +1,7 @@
 drop table if exists invoice;
 create table invoice(
     invoice_number bigint auto_increment primary key, 
-    invoice_type   char(1),
+    invoice_type   char(1) not null,
     invoice_date   varchar(20),
     total_amount   decimal(8, 2),
     net_amount     decimal(8,2),
