9aa992545a7f1c257ccc017a1dd03110b91bdc20
Jiang Liu <liujiang682@gmail.com>
2015-06-12 10:13:56 +1000

    More integration tests.

--- invoices/src/test/java/com/profectus/invoices/services/InvoicesServicesIT.java
+++ invoices/src/test/java/com/profectus/invoices/services/InvoicesServicesIT.java
@@ -1,6 +1,12 @@
 package com.profectus.invoices.services;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +27,20 @@ public class InvoicesServicesIT extends AbstractTransactionalJUnit4SpringContext
 	public void testFind() {
 		Invoice invoice = this.invoicesServices.find(1l);
 		assertNotNull(invoice);
+		assertTrue(1 == invoice.getInvoiceNumber());
+		assertEquals("c", invoice.getInvoiceType());
+		assertEquals("11/06/2015", invoice.getInvoiceDate());
+		assertEquals(new BigDecimal(330).setScale(2), invoice.getTotalAmount());
+		assertEquals(new BigDecimal(300).setScale(2), invoice.getNetAmount());
+		assertEquals("s", invoice.getCashMethod());
+		assertNull(invoice.getSecurityFee());
+	}
+	
+	@Test
+	public void testFindByInvoiceType() {
+		List<Invoice> invoices = this.invoicesServices.findByInvoiceType("c", 10);
+		assertNotNull(invoices);
+		assertTrue(invoices.size() == 10);
 	}
 	
 }
